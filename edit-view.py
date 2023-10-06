3651614edca0fb792dbf7cbcac65b2e3d3c9079b
blazej-smorawski <blazej.smorawski@gmail.com>
2023-10-06 09:31:19 +0200

    Update editor

--- edit-view.py
+++ edit-view.py
@@ -5,13 +5,13 @@ st.set_page_config(
     layout="wide"
 )
 
-import pandas as pd
-from utils.database import get_data, insert_data
+import json
 
-from code_editor import code_editor
+import yaml
 from utils.runner import run_code
-
-
+from code_editor import code_editor
+from utils.database import get_data, insert_data
+import pandas as pd
 # Improve page layout
 hide_streamlit_style = """
 <style>
@@ -21,41 +21,115 @@ hide_streamlit_style = """
 
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-st.markdown("# Edytor zadań")
+st.markdown("### Edytor zadań")
+
+if 'task' not in st.session_state:
+    st.session_state['task'] = {
+        "name": "Czarna magia",
+        "edition": "Python 2023",
+        "initial-code": "print(1)",
+        "description": "Magikuj",
+        "test-cases": [
+            {
+                "input": "1",
+                "output": "2",
+            }
+        ],
+    }
+
+task = st.session_state.task
 
 col1, col2 = st.columns([1, 2])
 with col1:
-    task = {}
-    task["name"] = st.text_input('Nazwa', 'Czarna magia')
-    task["edition"] = st.text_input('Edycja', 'Python 2023')
-    task["initial-code"] = st.text_area('Kod początkowy', 'print(1)')
-    task["description"] = st.text_area('Opis', 'Magikuj')
-    st.markdown(task["description"])
+    database, form, json_editor, yaml_editor = st.tabs(
+        ['Wybierz z bazy', 'Formularz', 'JSON', 'YAML'])
+    with database:
+        groups = get_data("editions", {"public": {"$eq": True}})
+        group_name = st.selectbox(
+                    'Wybierz edycję', [group['name'] for group in groups], label_visibility="collapsed")
 
-    df = pd.DataFrame(
-        [
-            {"input": "2\\n2", "output": "4", },
-        ]
-    )
-    edited_df = st.data_editor(
-        df, num_rows="dynamic", use_container_width=True)
+        tasks = get_data("tasks", {"edition": {"$eq": group_name}})
+        task_name = st.selectbox(
+            'Wybierz zadanie', [task["name"] for task in tasks], label_visibility="collapsed")
 
-    # try:
-    task["test-cases"] = []
-    for index in range(len(edited_df["input"])):
-        task["test-cases"].append(
-            {
-                "input": edited_df["input"][index],
-                "output": edited_df["output"][index]
-            }
-        )
-    # except:
-    #     st.error("Niepoprawny format przypadków testowych")
+        def load():
+            task = next(filter(lambda x: x["name"] == task_name, tasks))
+            del task["_id"]
+            st.session_state.task = task
+
+        st.button("Załaduj z bazy", on_click=load)
+
+    with form:
+        st.button("Odśwież", key="refresh_form")
+
+        task["name"] = st.text_input('Nazwa', task["name"])
+        task["edition"] = st.text_input('Edycja', task["edition"])
+        task["initial-code"] = st.text_area('Kod początkowy', task["initial-code"])
+        task["description"] = st.text_area('Opis', task["description"])
+
+        df = pd.DataFrame(task["test-cases"])
+        edited_df = st.data_editor(
+            df, num_rows="dynamic", use_container_width=True)
+
+        try:
+            task["test-cases"] = []
+            for index in range(len(edited_df["input"])):
+                task["test-cases"].append(
+                    {
+                        "input": edited_df["input"][index],
+                        "output": edited_df["output"][index]
+                    }
+                )
+        except:
+            st.warning("Invalid test cases!")
+
+    with json_editor:
+        st.button("Odśwież", key="refresh_json")
+
+        code = json.dumps(task, indent=4)
+        editor_buttons = [{
+            "name": "Zapisz",
+            "feather": "Save",
+            "primary": True,
+            "hasText": True,
+            "showWithIcon": True,
+            "commands": ["submit"],
+            "style": {"bottom": "0.44rem", "right": "0.4rem"},
+            "alwaysOn": True
+        }]
+        editor_response = code_editor(
+            code, lang="json", key=str(hash(json.dumps(task, sort_keys=True)))+"_json_editor", height=20, buttons=editor_buttons)
+
+        if editor_response['type'] == "submit":
+            st.session_state.task = json.loads(editor_response['text'])
+            task = st.session_state.task
+
+    with yaml_editor:
+        code = yaml.dump(task, indent=4)
+        editor_buttons = [{
+            "name": "Zapisz",
+            "feather": "Save",
+            "primary": True,
+            "hasText": True,
+            "showWithIcon": True,
+            "commands": ["submit"],
+            "style": {"bottom": "0.44rem", "right": "0.4rem"},
+            "alwaysOn": True
+        }]
+        editor_response = code_editor(
+            code, lang="yaml", key=str(hash(json.dumps(task, sort_keys=True)))+"_yaml_editor", height=20, buttons=editor_buttons)
+
+        if editor_response['type'] == "submit":
+            st.session_state.task = yaml.load(editor_response['text'])
+            task = st.session_state.task
+
     if st.button("Wyślij", type="primary"):
         insert_data("tasks", task)
 
 
 with col2:
+    st.markdown(f"### {task['name']}")
+    st.markdown(task["description"])
     code = task["initial-code"]
     editor_buttons = [{
         "name": "Uruchom",
@@ -64,10 +138,11 @@ with col2:
         "hasText": True,
         "showWithIcon": True,
         "commands": ["submit"],
-        "style": {"bottom": "0.44rem", "right": "0.4rem"}
+        "style": {"bottom": "0.44rem", "right": "0.4rem"},
+        "alwaysOn": True
     }]
     editor_response = code_editor(
-        code, key=task["name"]+str(task["test-cases"])+task["initial-code"]+"_editor", height=[10, 20], buttons=editor_buttons)
+        code, key=str(hash(json.dumps(task, sort_keys=True)))+"_editor", height=[10, 20], buttons=editor_buttons)
 
     if editor_response['type'] == "submit":
         run_code(task, editor_response["text"])
