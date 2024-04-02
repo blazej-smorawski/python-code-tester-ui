1144cc4901df5d0b75855980c1fbd5559a709f81
blazej-smorawski <blazej.smorawski@gmail.com>
2024-04-02 18:45:12 +0200

    Add basic auth to `edit-view`

--- edit-view.py
+++ edit-view.py
@@ -1,3 +1,4 @@
+import hmac
 from io import BytesIO
 import random
 import string
@@ -24,7 +25,33 @@ hide_streamlit_style = """
 
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-st.markdown("### Edytor zadań")
+st.markdown("### Admin UI")
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["admin_password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 if 'task' not in st.session_state:
     st.session_state['task'] = {
