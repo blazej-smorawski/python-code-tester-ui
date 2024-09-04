48619318dcc4552ee68de7e1a5a9d8359475884c
blazej-smorawski <blazej.smorawski@intel.com>
2024-09-04 17:40:55 +0200

    Layout experiments

--- test-code.py
+++ test-code.py
@@ -9,17 +9,23 @@ from streamlit_extras.stylable_container import stylable_container
 
 _RELEASE = True
 
+pc = st.get_option('theme.primaryColor')
+bc = st.get_option('theme.backgroundColor')
+sbc = st.get_option('theme.secondaryBackgroundColor')
+tc = st.get_option('theme.textColor')
+
 display_navbar()
 
 _, center, _ = st.columns([1, 5, 1])
 with center:
     render_front_page(_RELEASE)
     
+    st.title("Hello World! 🌎")
     left, right = st.columns([4, 6])
 
     with left:
         editor_buttons = [{
-            "name": "Generuj stronę!",
+            "name": "Uruchom Program",
             "feather": "Play",
             "primary": True,
             "hasText": True,
@@ -30,45 +36,69 @@ with center:
         }]
 
         editor_response = code_editor(
-            "print('# Hello world!🌎')", key="_frontpage_editor", height=[10, 20], buttons=editor_buttons)
+            "print('Hello world!')", key="_frontpage_editor", height=[10, 20], buttons=editor_buttons)
         if editor_response['type'] == "submit":
             result = run_code(editor_response["text"], "")
-            st.session_state['result_1']=result
+            display_run_result(result)
 
     with right:
-        pc = st.get_option('theme.primaryColor')
-        bc = st.get_option('theme.backgroundColor')
-        sbc = st.get_option('theme.secondaryBackgroundColor')
-        tc = st.get_option('theme.textColor')
-        if 'result_1' in st.session_state:
-            with stylable_container(key="generated_page", css_styles=f"""
-                            {{
-                                background-color: {sbc};
-                                z-index: 999;
-                                padding: 10px;
-                                border-style: solid;
-                                border-width: 1px;
-                                border-radius: 5px;
-                                border-color: {pc};
-                                border-radius: 5px;
-                            }}
-                            """):
-                st.markdown(st.session_state['result_1']['output'])
-        else:
-            with stylable_container(key="generated_page", css_styles=f"""
-                            {{
-                                background-color: {sbc};
-                                z-index: 999;
-                                padding: 10px;
-                                border-style: solid;
-                                border-width: 1px;
-                                border-radius: 5px;
-                                border-color: {pc};
-                                border-radius: 5px;
-                                text-align: center;
-                            }}
-                            """):
-                st.markdown("# ???")
+        with stylable_container(key="generated_page", css_styles=f"""
+                        {{
+                            background-color: {sbc};
+                            z-index: 999;
+                            padding: 10px;
+                            border-style: solid;
+                            border-width: 1px;
+                            border-radius: 5px;
+                            border-color: {pc};
+                            border-radius: 5px;
+                        }}
+                        """):
+            st.markdown("""
+            ### Lorem
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam libero elit, pellentesque eget efficitur sed, bibendum et nulla. Sed faucibus dolor lectus, pretium gravida eros imperdiet in. Duis pretium ac metus sed iaculis. Proin id molestie elit. Curabitur fermentum, leo ut porttitor congue, odio felis rutrum erat, vel tempor sem velit in nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis porta turpis a egestas lobortis. Phasellus interdum neque a ipsum ultrices facilisis. Duis ligula erat, blandit ut convallis sed, vulputate non neque. Curabitur eu malesuada elit. In ac massa at neque placerat dignissim. Vestibulum sed ullamcorper est, vestibulum dignissim augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius ornare lectus in vestibulum.
+            """)
+
+    st.divider()
+    left, right = st.columns([6, 4])
+
+    with left:
+        with stylable_container(key="generated_page_2", css_styles=f"""
+                        {{
+                            background-color: {sbc};
+                            z-index: 999;
+                            padding: 10px;
+                            border-style: solid;
+                            border-width: 1px;
+                            border-radius: 5px;
+                            border-color: {pc};
+                            border-radius: 5px;
+                        }}
+                        """):
+            with st.container(height=300, border=False):
+                st.markdown("""
+                ### Hello
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam libero elit, pellentesque eget efficitur sed, bibendum et nulla. Sed faucibus dolor lectus, pretium gravida eros imperdiet in. Duis pretium ac metus sed iaculis. Proin id molestie elit. Curabitur fermentum, leo ut porttitor congue, odio felis rutrum erat, vel tempor sem velit in nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis porta turpis a egestas lobortis. Phasellus interdum neque a ipsum ultrices facilisis. Duis ligula erat, blandit ut convallis sed, vulputate non neque. Curabitur eu malesuada elit. In ac massa at neque placerat dignissim. Vestibulum sed ullamcorper est, vestibulum dignissim augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius ornare lectus in vestibulum.
+                """)
+
+    with right:
+        with stylable_container(key="generated_page_2", css_styles=f"""
+                        {{
+                            background-color: {sbc};
+                            z-index: 999;
+                            padding: 10px;
+                            border-style: solid;
+                            border-width: 1px;
+                            border-radius: 5px;
+                            border-color: {pc};
+                            border-radius: 5px;
+                        }}
+                        """):
+            with st.container(height=300, border=False):
+                st.markdown("""
+                ### Tak
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam libero elit, pellentesque eget efficitur sed, bibendum et nulla. Sed faucibus dolor lectus, pretium gravida eros imperdiet in. Duis pretium ac metus sed iaculis. Proin id molestie elit. Curabitur fermentum, leo ut porttitor congue, odio felis rutrum erat, vel tempor sem velit in nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis porta turpis a egestas lobortis. Phasellus interdum neque a ipsum ultrices facilisis. Duis ligula erat, blandit ut convallis sed, vulputate non neque. Curabitur eu malesuada elit. In ac massa at neque placerat dignissim. Vestibulum sed ullamcorper est, vestibulum dignissim augue.
+                """)
 
     st.markdown('''
         ## Regulamin
