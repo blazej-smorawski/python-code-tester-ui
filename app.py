ec604358b88ddacdf5ef434e9d8d05fb706ae031
blazej-smorawski <blazej.smorawski@intel.com>
2024-09-09 18:02:16 +0200

    Blog initial

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from src.blog import display_blog
 
 pc = st.get_option('theme.primaryColor')
 bc = st.get_option('theme.backgroundColor')
@@ -11,15 +12,12 @@ st.set_page_config(
             page_icon="🧙‍♂️"
         )
 
-def page2():
-    st.title("Second page")
-
 pg = st.navigation([
     st.Page("src/front.py", title="Pomorski Czarodziej", icon="🧙‍♂️"),
     st.Page("src/blog.py", title="Blog", icon="📰"),
+    st.Page(lambda: display_blog("# TEST", [{}]), title="Blog", icon="📰", url_path="blog+a"),
     st.Page("src/competition.py", title="Konkurs", icon="📝"),
     st.Page("src/ide.py", title="Programuj!", icon="⌨️"),
     st.Page("src/training.py", title="Zbiór Zadań", icon="📚"),
-    st.Page(page2, title="Second page", icon=":material/favorite:", url_path="test",),
 ], position="hidden")
 pg.run()
--- src/blog.py
+++ src/blog.py
@@ -1,48 +1,53 @@
 
 import streamlit as st
-from code_editor import code_editor
 from utils.navbar import display_navbar
-from utils.runner import display_testcase_result, test_code
 from utils.database import get_data
+from streamlit_extras.stylable_container import stylable_container
 
-display_navbar()
+def display_blog(text, links):
+    display_navbar()
 
-groups = get_data("editions", {"public": {"$eq": True}})
-groups_names = [group["name"] for group in groups]
-tabs = st.tabs(groups_names)
-# Add tab
-for element in zip(groups, tabs):
-    element[0]["tab"] = element[1]
+    pc = st.get_option('theme.primaryColor')
+    bc = st.get_option('theme.backgroundColor')
+    sbc = st.get_option('theme.secondaryBackgroundColor')
+    tc = st.get_option('theme.textColor')
 
-# Editions
-for group in groups:
-    with group["tab"]:
-        tasks = get_data("tasks", {"edition": {"$eq": group["name"]}})
-
-        col1, col2 = st.columns([1, 2])
-        with col1:
-            task_name = st.selectbox(
-                'Wybierz zadanie', [task["name"] for task in tasks], label_visibility="collapsed")
-            task = next(filter(lambda x: x["name"] == task_name, tasks))
-            st.markdown(f"### {task_name}")
-            st.write(task["description"])
-
-        with col2:
-            code = task["initial-code"]
-            editor_buttons = [{
-                "name": "Uruchom",
-                "feather": "Play",
-                "primary": True,
-                "hasText": True,
-                "showWithIcon": True,
-                "commands": ["submit"],
-                "style": {"bottom": "0.44rem", "right": "0.4rem"},
-                "alwaysOn": True
-            }]
-            editor_response = code_editor(
-                code, key=group["name"]+task["name"]+"_editor", height=[10, 20], buttons=editor_buttons)
-
-            if editor_response['type'] == "submit":
-                results = test_code(task, editor_response["text"])
-                for result in results:
-                    display_testcase_result(result)
+    bar, _, blog = st.columns([2, 1 ,10])
+    with bar:
+        with stylable_container(key="blog_links", css_styles=f"""
+                            {{
+                                background-color: {sbc};
+                                padding: 10px;
+                                border-style: solid;
+                                border-width: 1px;
+                                border-radius: 5px;
+                                border-color: {pc};
+                                border-radius: 5px;
+                                div{{
+                                    width:90%;
+                                }}
+                            }}
+                            """):
+            st.write("### 2024")
+            st.page_link(
+                "src/ide.py", label="Programuj!", icon="⌨️", use_container_width=False)
+            st.page_link(
+                "src/ide.py", label="Programuj!", icon="⌨️", use_container_width=False)
+            st.page_link(
+                "src/ide.py", label="Programuj!", icon="⌨️", use_container_width=False)
+    with blog:
+        with stylable_container(key="blogasddsa", css_styles=f"""
+                        {{
+                            background-color: {sbc};
+                            padding: 10px;
+                            border-style: solid;
+                            border-width: 1px;
+                            border-radius: 5px;
+                            border-color: {pc};
+                            border-radius: 5px;
+                            div{{
+                                width:90%;
+                            }}
+                        }}
+                        """):
+            st.markdown(text)
