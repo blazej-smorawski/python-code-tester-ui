b9aa3fa36ef6211e35ca1cb11da856e20bfddae6
blazej-smorawski <blazej.smorawski@gmail.com>
2024-09-22 12:38:32 +0200

    Improve language style

--- src/blogs/blog_17_10_2024.py
+++ src/blogs/blog_17_10_2024.py
@@ -1,33 +1,33 @@
 from src.blog import display_blog
 
 text="""
-### Cześć młodzi programiści! 👋 
+### Cześć, młodzi programiści! 👋 
 
-Z radością ogłaszamy start naszego nowego serwisu, stworzonego specjalnie z myślą o Was - uczniach szkół podstawowych, którzy chcą spróbować swoich sił w programowaniu. Nasz konkurs, Pomorski Czarodziej, to nie tylko okazja do rywalizacji, ale przede wszystkim świetna zabawa i nauka poprzez praktyczne zadania.
+Z radością ogłaszamy start naszego nowego serwisu, stworzonego specjalnie z myślą o Was – uczniach szkół podstawowych, którzy chcą spróbować swoich sił w programowaniu. Nasz konkurs, Pomorski Czarodziej, to nie tylko okazja do rywalizacji, ale przede wszystkim świetna zabawa i nauka poprzez praktyczne zadania.
 
 #### Co oferuje nasz serwis?
 
 Nasz serwis to kompletny ekosystem, w którym możecie doskonalić swoje umiejętności programistyczne w języku Python. Oto, co znajdziecie na naszej stronie:
 
-* Programuj! 🖥️
+* **Programuj!** 🖥️
 
-    Aby rozpocząć przygodę z programowaniem, nie musicie instalować żadnych dodatkowych programów. W zakładce Programuj! czeka na Was gotowe środowisko programistyczne, gdzie możecie pisać i uruchamiać swoje kody od razu w przeglądarce! Pierwszy kod? To oczywiście magiczne „Hello World! 🌎”, które otworzy przed Wami drzwi do świata pełnego możliwości.
+    Aby rozpocząć przygodę z programowaniem, nie musicie instalować żadnych dodatkowych programów. W zakładce *Programuj!* czeka na Was gotowe środowisko programistyczne, gdzie możecie pisać i uruchamiać swoje programy bezpośrednio w przeglądarce! Pierwszy program? To oczywiście magiczne „Hello World! 🌎”, które otworzy przed Wami drzwi do świata pełnego możliwości.
 
-* Zbiór Zadań 📚
+* **Zbiór Zadań** 📚
 
-    W zakładce Zbiór Zadań znajdziecie zestaw zadań z poprzednich edycji konkursu, idealnych do ćwiczeń i sprawdzenia swojej wiedzy. Dzięki naszemu serwisowi, każde rozwiązane zadanie zostanie natychmiast sprawdzone, a Wy zobaczycie, co jeszcze można poprawić. Ćwiczcie, rozwiązujcie zadania i przygotujcie się na wyzwania konkursowe!
+    W zakładce *Zbiór Zadań* znajdziecie zestaw zadań z poprzednich edycji konkursu, idealnych do ćwiczeń i sprawdzenia swojej wiedzy. Dzięki naszemu serwisowi każde rozwiązane zadanie zostanie natychmiast sprawdzone, a Wy zobaczycie, co jeszcze można poprawić. Ćwiczcie, rozwiązujcie zadania i przygotujcie się na wyzwania konkursowe!
 
-* Konkurs 📝
+* **Konkurs** 📝
 
-    Najważniejsza zakładka pojawi się w dniu konkursu. Po wprowadzeniu identyfikatora otrzymanego od nauczyciela, będziecie mieli godzinę na rozwiązanie kilku zadań konkursowych. Czy jesteście gotowi na wyzwanie? Wasz czas startuje, a my trzymamy kciuki za wszystkie Wasze kody i rozwiązania!
+    Najważniejsza zakładka pojawi się w dniu konkursu. Po wprowadzeniu identyfikatora otrzymanego od nauczyciela będziecie mieli godzinę na rozwiązanie kilku zadań konkursowych. Czy jesteście gotowi na wyzwanie? Wasz czas startuje, a my trzymamy kciuki za wszystkie Wasze kody i rozwiązania!
 
 ### Wszystkie informacje w jednym miejscu
 
-Nasz konkurs jest skierowany do uczniów szkół podstawowych z Pomorza. Wszystkie aktualności i informacje o tegorocznej edycji możecie znaleźć w zakładce 📰Aktualności na naszej stronie.
+Nasz konkurs jest skierowany do uczniów szkół podstawowych z Pomorza. Wszystkie aktualności i informacje o tegorocznej edycji znajdziecie w zakładce 📰 *Aktualności* na naszej stronie.
 
-Pełen regulamin konkursu na rok 2024 możecie pobrać klikając w przycisk poniżej. Zapoznajcie się z nim dokładnie, aby wiedzieć, jak najlepiej przygotować się do udziału.
+Pełen regulamin konkursu na rok 2024 możecie pobrać, klikając w przycisk poniżej. Zapoznajcie się z nim dokładnie, aby wiedzieć, jak najlepiej przygotować się do udziału.
 
-Zapraszamy Was do świata programowania - zacznijcie od prostego „Hello World” i zobaczcie, dokąd Was to zaprowadzi. Czekamy na Wasze kreatywne rozwiązania i nie możemy doczekać się, by zobaczyć, jakie cuda stworzycie z kodu!
+Zapraszamy Was do świata programowania – zacznijcie od prostego „Hello World!” i zobaczcie, dokąd Was to zaprowadzi. Czekamy na Wasze kreatywne rozwiązania i nie możemy się doczekać, by zobaczyć, jakie cuda stworzycie za pomocą Pythona!
 
 #### Do zobaczenia na konkursie i powodzenia! 🎉
 """
--- src/front.py
+++ src/front.py
@@ -14,7 +14,7 @@ tc = st.get_option('theme.textColor')
 display_navbar()
 
 _, center, _ = st.columns([1, 5, 1])
-with center:    
+with center:
     st.title("Hello World! 🌎")
     left, right = st.columns([4, 6])
 
@@ -31,7 +31,7 @@ with center:
         }]
 
         editor_response = code_editor(
-            "print('Hello world!')", key="_frontpage_editor", height=[10, 20], buttons=editor_buttons)
+            "print('Hello World!')", key="_frontpage_editor", height=[10, 20], buttons=editor_buttons)
         if editor_response['type'] == "submit":
             result = run_code(editor_response["text"], "")
             display_run_result(result)
@@ -45,23 +45,22 @@ with center:
                             border-width: 1px;
                             border-radius: 5px;
                             border-color: {pc};
-                            border-radius: 5px;
-                            div{{
-                                width:90%;
+                            div {{
+                                width: 90%;
                             }}
                         }}
                         """):
             st.markdown("""
-            ### Cześć młodzi programiści!👋
+            ### Cześć, młodzi programiści! 👋
             Zapraszamy Was do świata programowania, gdzie wszystko zaczyna się od magicznego :rainbow[**Hello World**]. To pierwsze kroki, które prowadzą do tworzenia gier, aplikacji i wszystkiego, co tylko sobie wymarzycie. Nasz konkurs to świetna okazja, by spróbować swoich sił i zobaczyć, jakie cuda można wyczarować z kodu. Dołączcie do nas, rozwijając swoje umiejętności, kreatywność i pracę zespołową.
             """)
 
-    #divider()
+    # divider()
     
-    st.title("Pomorski czarodziej")
-    st.markdown("Nasz konkurs jest skierowany do uczniów szkół podstawowych z pomorza. Wszystkie informacje o tegorocznej edycji możecie znaleźć w zakładce **📰Aktualności**.")
+    st.title("Pomorski Czarodziej")
+    st.markdown("Nasz konkurs jest skierowany do uczniów szkół podstawowych z pomorza. Wszystkie informacje o tegorocznej edycji możecie znaleźć w zakładce **📰 Aktualności**.")
     
-    #st.divider()
+    # st.divider()
     left, center, right = st.columns([3, 3, 3])
 
     with left:
@@ -73,17 +72,17 @@ with center:
                             border-width: 1px;
                             border-radius: 5px;
                             border-color: {pc};
-                            border-radius: 5px;
-                            div{{
-                                width:90%;
+                            div {{
+                                width: 90%;
                             }}
                         }}
                         """):
             st.page_link(
                 "src/ide.py", label="Programuj!", icon="⌨️", use_container_width=False)
             st.markdown("""
-                        Aby ułatwić wam dostęp do programowania, nasza strona udostępnia gotowe środowisko programistyczne języka Python w zakładce **Programuj!**
+                        Aby ułatwić Wam dostęp do programowania, nasza strona udostępnia gotowe środowisko programistyczne języka Python w zakładce **Programuj!**
                         """)
+
     with center:
         with stylable_container(key="generated_page_3", css_styles=f"""
                         {{
@@ -93,20 +92,19 @@ with center:
                             border-width: 1px;
                             border-radius: 5px;
                             border-color: {pc};
-                            border-radius: 5px;
-                            div{{
-                                width:90%;
+                            div {{
+                                width: 90%;
                             }}
                         }}
                         """):
             st.page_link(
                 "src/training.py", label="Zbiór Zadań", icon="📚", use_container_width=False)
             st.markdown("""
-                        Aby ułatwić wam przygotowanie do konkursu, w zakładce **Zbiór Zadań** przygotowaliśmy dla Was zadania z poprzednich edycji. Wszystkie zadania są automatycznie sprawdzane przez nasz serwis, więc śmiało możecie sprawdzić swoją wiedzę 🎯.
+                        Aby ułatwić Wam przygotowanie do konkursu, w zakładce **Zbiór Zadań** przygotowaliśmy dla Was zadania z poprzednich edycji. Wszystkie zadania są automatycznie sprawdzane przez nasz serwis, więc śmiało możecie sprawdzić swoją wiedzę 🎯.
                         """)
 
     with right:
-        with stylable_container(key="generated_page_4", css_styles=f"""                        
+        with stylable_container(key="generated_page_4", css_styles=f"""
                         {{
                             background-color: {sbc};
                             padding: 10px;
@@ -114,21 +112,20 @@ with center:
                             border-width: 1px;
                             border-radius: 5px;
                             border-color: {pc};
-                            border-radius: 5px;
-                            div{{
-                                width:90%;
+                            div {{
+                                width: 90%;
                             }}
                         }}
                         """):
             st.page_link(
                 "src/competition.py", label="Konkurs", icon="📝", use_container_width=False)
             st.markdown("""
-                        W dniu konkursu zakładka **Konkurs** będzie otwarta dla wszystkich chętnych! Po wprowadzeniu identyfikatora otrzymanego od nauczyciela, będziecie mieć godzinę na rozwiązanie kilku zadań podobnych to tych, ze zbioru zadań. Powodzenia!
+                        W dniu konkursu zakładka **Konkurs** będzie otwarta dla wszystkich chętnych! Po wprowadzeniu identyfikatora otrzymanego od nauczyciela będziecie mieli godzinę na rozwiązanie kilku zadań podobnych do tych ze *Zbioru Zadań*. Powodzenia!
                         """)
 
     st.markdown('''
         ## Regulamin
-        Regulamin konkursu na rok 2024 możesz ściągnąć klikając w poniższy przycisk
+        Regulamin konkursu na rok 2024 możecie pobrać, klikając w poniższy przycisk.
     ''')
 
     with open("docs/Pomorski Czarodziej 2024 - Regulamin.pdf", "rb") as file:
